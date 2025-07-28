912a760dad20f03e1159f8ebb5e033bcd5f62de1
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-07-28 21:27:33 +0100

    Add files via upload

--- bytecodellvm.c
+++ bytecodellvm.c
@@ -85,6 +85,7 @@ void gerar_llvm(const char *input_filename, const char *output_filename) {
 
 int main() {
     char input[256], output[256];
+    printf("\033c\033[43;30m\n");
     printf("Ficheiro C de entrada: ");
     scanf("%s", input);
     sprintf(output, "%.*s.ll", (int)(strlen(input) - 2), input);
